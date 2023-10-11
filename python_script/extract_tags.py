2a15c36738a3452599b9fb7655928857f66455ac
mniu <122190401+mniu@users.noreply.github.com>
2023-10-11 16:47:56 -0500

    Create extract_tags.py
    Please refer to the following link to obtain this python code from the NanoSeq ms.
    
    https://github.com/cancerit/NanoSeq/blob/fd22dec943a3a9cc70643f079c40a8baf13a8e62/python/extract_tags.py

---
+++ python_script/extract_tags.py
@@ -0,0 +1,2 @@
+##Please refer to the following link to obtain this python code from the NanoSeq ms.
+#https://github.com/cancerit/NanoSeq/blob/fd22dec943a3a9cc70643f079c40a8baf13a8e62/python/extract_tags.py
